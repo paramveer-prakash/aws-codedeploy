4619868b7e081adf0c6376938212af272c53fe78
Paramveer Prakash <paramveer.prakash@gmail.com>
2025-03-08 09:52:10 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,18 @@
 # Login to AWS ECR
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 200174251764.dkr.ecr.ap-south-1.amazonaws.com
 
-# Pull the latest Docker image from ECR
+# Pull the latest Docker images from ECR
 docker pull 200174251764.dkr.ecr.ap-south-1.amazonaws.com/masterproject/api-core:latest
+docker pull 200174251764.dkr.ecr.ap-south-1.amazonaws.com/masterproject/app-core:latest
 
-# Stop the existing container
+
+# Stop the existing containers
 docker stop api-core || true
 docker rm api-core || true
+docker stop app-core || true
+docker rm app-core || true
 
-# Run the new container
+
+# Run the new containers
 docker run -d -p 8080:8080 --name api-core 200174251764.dkr.ecr.ap-south-1.amazonaws.com/masterproject/api-core:latest
+docker run -d -p 4200:4200 --name app-core 200174251764.dkr.ecr.ap-south-1.amazonaws.com/masterproject/app-core:latest
